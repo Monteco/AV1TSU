cec20476d9afdda08a59df345c28673b07f5788c
Monteco <33065620+Monteco@users.noreply.github.com>
2019-04-01 23:19:28 +0300

    0.01

---
+++ AV1TSUx0003/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace AV1TSUx0003
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputLocation = new System.Windows.Forms.TextBox();
+            this.OutputLocation = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Encoder = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ConstantQuality = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Multithreading = new System.Windows.Forms.ComboBox();
+            this.PixelFormat = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Tiles = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AudioBitrate = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Resolution = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Speed = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ConstantQuality)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AudioBitrate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Speed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputLocation
+            // 
+            this.InputLocation.Location = new System.Drawing.Point(13, 13);
+            this.InputLocation.Name = "InputLocation";
+            this.InputLocation.Size = new System.Drawing.Size(623, 20);
+            this.InputLocation.TabIndex = 0;
+            this.InputLocation.TextChanged += new System.EventHandler(this.InputLocation_TextChanged);
+            // 
+            // OutputLocation
+            // 
+            this.OutputLocation.Location = new System.Drawing.Point(13, 40);
+            this.OutputLocation.Name = "OutputLocation";
+            this.OutputLocation.Size = new System.Drawing.Size(623, 20);
+            this.OutputLocation.TabIndex = 1;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(642, 10);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(75, 23);
+            this.Input.TabIndex = 2;
+            this.Input.Text = "Input";
+            this.Input.UseVisualStyleBackColor = true;
+            this.Input.Click += new System.EventHandler(this.Input_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(642, 40);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(75, 23);
+            this.Output.TabIndex = 3;
+            this.Output.Text = "Output";
+            this.Output.UseVisualStyleBackColor = true;
+            this.Output.Click += new System.EventHandler(this.Output_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(642, 298);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Encoder";
+            // 
+            // Encoder
+            // 
+            this.Encoder.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Encoder.FormattingEnabled = true;
+            this.Encoder.Items.AddRange(new object[] {
+            "libaom-av1",
+            "rav1e"});
+            this.Encoder.Location = new System.Drawing.Point(13, 80);
+            this.Encoder.Name = "Encoder";
+            this.Encoder.Size = new System.Drawing.Size(121, 21);
+            this.Encoder.TabIndex = 0;
+            this.Encoder.SelectedIndexChanged += new System.EventHandler(this.Encoder_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Speed";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Constant Quality";
+            // 
+            // ConstantQuality
+            // 
+            this.ConstantQuality.Location = new System.Drawing.Point(269, 80);
+            this.ConstantQuality.Maximum = new decimal(new int[] {
+            63,
+            0,
+            0,
+            0});
+            this.ConstantQuality.Name = "ConstantQuality";
+            this.ConstantQuality.Size = new System.Drawing.Size(120, 20);
+            this.ConstantQuality.TabIndex = 9;
+            this.ConstantQuality.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.ConstantQuality.ValueChanged += new System.EventHandler(this.ConstantQuality_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(395, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Multithreading";
+            // 
+            // Multithreading
+            // 
+            this.Multithreading.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Multithreading.FormattingEnabled = true;
+            this.Multithreading.Items.AddRange(new object[] {
+            "False",
+            "True"});
+            this.Multithreading.Location = new System.Drawing.Point(398, 80);
+            this.Multithreading.Name = "Multithreading";
+            this.Multithreading.Size = new System.Drawing.Size(121, 21);
+            this.Multithreading.TabIndex = 11;
+            this.Multithreading.SelectedIndexChanged += new System.EventHandler(this.Multithreading_SelectedIndexChanged);
+            // 
+            // PixelFormat
+            // 
+            this.PixelFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PixelFormat.FormattingEnabled = true;
+            this.PixelFormat.Items.AddRange(new object[] {
+            "yuv420p      ",
+            "yuv422p",
+            "yuv444p",
+            "yuv420p10le",
+            "yuv422p10le",
+            "yuv444p10le",
+            "yuv420p12le",
+            "yuv422p12le",
+            "yuv444p12le"});
+            this.PixelFormat.Location = new System.Drawing.Point(15, 147);
+            this.PixelFormat.Name = "PixelFormat";
+            this.PixelFormat.Size = new System.Drawing.Size(121, 21);
+            this.PixelFormat.TabIndex = 12;
+            this.PixelFormat.SelectedIndexChanged += new System.EventHandler(this.PixelFormat_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Pixel Format";
+            // 
+            // Tiles
+            // 
+            this.Tiles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tiles.FormattingEnabled = true;
+            this.Tiles.Items.AddRange(new object[] {
+            "4x1",
+            "2x2"});
+            this.Tiles.Location = new System.Drawing.Point(141, 147);
+            this.Tiles.Name = "Tiles";
+            this.Tiles.Size = new System.Drawing.Size(121, 21);
+            this.Tiles.TabIndex = 14;
+            this.Tiles.SelectedIndexChanged += new System.EventHandler(this.Tiles_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(141, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Tiles";
+            // 
+            // AudioBitrate
+            // 
+            this.AudioBitrate.Location = new System.Drawing.Point(269, 147);
+            this.AudioBitrate.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.AudioBitrate.Name = "AudioBitrate";
+            this.AudioBitrate.Size = new System.Drawing.Size(120, 20);
+            this.AudioBitrate.TabIndex = 16;
+            this.AudioBitrate.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.AudioBitrate.ValueChanged += new System.EventHandler(this.AudioBitrate_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(269, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Audio Bitrate";
+            // 
+            // Resolution
+            // 
+            this.Resolution.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Resolution.FormattingEnabled = true;
+            this.Resolution.Items.AddRange(new object[] {
+            "Source",
+            "144",
+            "240",
+            "360",
+            "480",
+            "720",
+            "1080",
+            "1440",
+            "2160"});
+            this.Resolution.Location = new System.Drawing.Point(398, 146);
+            this.Resolution.Name = "Resolution";
+            this.Resolution.Size = new System.Drawing.Size(121, 21);
+            this.Resolution.TabIndex = 18;
+            this.Resolution.SelectedIndexChanged += new System.EventHandler(this.Resolution_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(398, 130);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Resolution";
+            // 
+            // Speed
+            // 
+            this.Speed.Location = new System.Drawing.Point(141, 80);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(120, 20);
+            this.Speed.TabIndex = 20;
+            this.Speed.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Speed.ValueChanged += new System.EventHandler(this.Speed_ValueChanged);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 333);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Resolution);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AudioBitrate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Tiles);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PixelFormat);
+            this.Controls.Add(this.Multithreading);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ConstantQuality);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Encoder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.OutputLocation);
+            this.Controls.Add(this.InputLocation);
+            this.Name = "Window";
+            this.Text = "AV1TSUx0003";
+            this.Load += new System.EventHandler(this.Window_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ConstantQuality)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AudioBitrate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Speed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputLocation;
+        private System.Windows.Forms.TextBox OutputLocation;
+        private System.Windows.Forms.Button Input;
+        private System.Windows.Forms.Button Output;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Encoder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown ConstantQuality;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Multithreading;
+        private System.Windows.Forms.ComboBox PixelFormat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox Tiles;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown AudioBitrate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox Resolution;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown Speed;
+    }
+}
+
---
+++ AV1TSUx0003/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AV1TSUx0003
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+            Encoder.SelectedIndex = 0;
+            Speed.Minimum = 1;
+            Speed.Maximum = 8;
+            Multithreading.SelectedIndex = 1;
+            PixelFormat.SelectedIndex = 3;
+            Tiles.SelectedIndex = 0;
+            Resolution.SelectedIndex = 0;
+        }
+        private void Window_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Input_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Input = new OpenFileDialog();
+            Input.ShowDialog();
+            InputLocation.Text = Input.FileName;
+        }
+
+        private void InputLocation_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Output_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Output = new SaveFileDialog();
+            Output.Filter = "MKV|*.mkv|WEBM|*.webm";
+            Output.ShowDialog();
+            OutputLocation.Text = Output.FileName;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string command = "";
+            if(Encoder.Text == "libaom-av1")
+            {
+                ProcessStartInfo StartInfo = new ProcessStartInfo();
+                command += " -i " + InputLocation.Text;
+                command += " -strict experimental -c:v " + Encoder.Text;
+                command += " -cpu-used " + Speed.Text;
+                command += " -crf " + ConstantQuality.Text.ToString() + " -b:v 0";
+                if (Resolution.SelectedIndex > 0)
+                {
+                    command += " -vf scale=" + Resolution.Text.ToString() + ":-1 ";
+                }
+                command += " -row-mt " + Multithreading.SelectedIndex.ToString();
+                command += " -pix_fmt " + PixelFormat.Text;
+                command += " -tiles " + Tiles.Text;
+                command += " -c:a libopus -ac 2 -b:a " + AudioBitrate.Text.ToString() + "k";
+                command += " -vbr on" + " -compression_level 10";
+                command += " " + OutputLocation.Text;
+                StartInfo.Arguments = command;
+                StartInfo.FileName = "ffmpeg.exe";
+                Process.Start(StartInfo).WaitForExit();
+            }
+            else if(Encoder.Text == "rav1e")
+            {
+                command = "";
+                ProcessStartInfo StartInfo = new ProcessStartInfo();
+                command += "/C ffmpeg.exe -i " + InputLocation.Text;
+                command += " -f yuv4mpegpipe - | rav1e - ";
+                command += " --limit 0";
+                command += " --quantizer 100";
+                command += " --speed " + Speed.Text.ToString();
+                command += " --output video.ivf";
+                StartInfo.Arguments = command;
+                StartInfo.FileName = "cmd.exe";
+                Process.Start(StartInfo).WaitForExit();
+                command = "";
+                command += " -i " + InputLocation.Text;
+                command += " -c:a libopus -ac 2 -b:a " + AudioBitrate.Text.ToString() + "k";
+                command += " -vbr on" + " -compression_level 10";
+                command += " audio.opus";
+                StartInfo.Arguments = command;
+                StartInfo.FileName = "ffmpeg.exe";
+                Process.Start(StartInfo).WaitForExit();
+                command = "";
+                command += "ffmpeg -i video.ivf -i audio.opus -c copy " + OutputLocation.Text;
+                StartInfo.Arguments = command;
+                StartInfo.FileName = "ffmpeg.exe";
+                Process.Start(StartInfo).WaitForExit();
+            }
+        }
+
+
+
+        private void CQ_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConstantQuality_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Multithreading_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PixelFormat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AudioBitrate_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Resolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Encoder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(Encoder.Text == "rav1e")
+            {
+                Speed.Maximum = 10;
+                Speed.Minimum = 0;
+                ConstantQuality.Maximum = 255;
+                ConstantQuality.Minimum = 0;
+                PixelFormat.Enabled = false;
+                Tiles.Enabled = false;
+                Multithreading.Enabled = false;
+                Resolution.Enabled = false;
+            }
+            if(Encoder.Text == "libaom-av1")
+            {
+                Speed.Maximum = 8;
+                Speed.Minimum = 1;
+                ConstantQuality.Maximum = 63;
+                ConstantQuality.Minimum = 1;
+                PixelFormat.Enabled = true;
+                Tiles.Enabled = true;
+                Multithreading.Enabled = true;
+                Resolution.Enabled = true;
+            }
+        }
+
+        private void Speed_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
